a25ab36c515c1831097ff4e46ae9be4dd6515c91
kmlst <80637401+kmlst@users.noreply.github.com>
2023-04-06 20:27:29 +0200

    continuation

--- tests/seance 2/test_min_power_temps.py
+++ tests/seance 2/test_min_power_temps.py
@@ -9,22 +9,22 @@ from graph import Graph
 class Test_ExecutionTime(unittest.TestCase):
 
     def test_temps_routes2(self):
-            g = Graph.graph_from_file(f"input/network.{2}.in")
-            g = g.kruskal()
-            with open(f"input/routes.{2}.in", 'r') as route:
-                route.readline()
-                duration = 0
-                for _ in range(5):
-                    ligne = route.readline()
-                    ligne.split(" ")
-                    print(ligne)
-                    node_a, node_b, utility = map(int, ligne)
-                    start = perf_counter()
-                    g.min_power_tree(node_a, node_b)
-                    end = perf_counter()
-                    duration += (end-start)
-                duration = duration/60
-                print(f"It would take roughly {N/5*duration:.4f} mins to compute min_power for all paths from network.{2}, (N={N})")
+        i = 2
+        g = Graph.graph_from_file(f"input/network.{i}.in")
+        g = g.kruskal()
+        with open(f"input/routes.{i}.in", 'r') as route:
+            N = int(route.readline())
+            duration = 0
+            k = 1
+            for _ in range(k):
+                ligne = route.readline().split(" ")
+                node_a, node_b, utility = map(int, ligne)
+                start = perf_counter()
+                g.min_power_optimized(node_a, node_b)
+                end = perf_counter()
+                duration += (end-start)
+            duration = duration/60
+            print(f"It would take roughly {(N/k)*duration:.4f} mins to compute min_power for all paths from network.{i}, (N={N})")
 
 
 if __name__ == '__main__':
