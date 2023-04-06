86f945318e718464efa9cc7380418ad774999057
kmlst <80637401+kmlst@users.noreply.github.com>
2023-04-06 18:31:07 +0200

    test

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -191,7 +191,7 @@ def route_proccessing(i, trucks, filewrite=False):
         for line in f.readlines(): # readlines is a generator
             a, b, utility, pmin = map(int, line.split(" ")) 
             # cost is the closest key, on the right
-            ind = bisect.bisect_right(trucks, pmin)
+            ind = bisect.bisect_right(trucks, pmin, key=lambda x:x[0])
             if ind < n_trucks-1: # route with too great power necessary
                 cost = trucks[ind][1]
                 routes.append(a, b, trucks[ind][0], utility, cost, utility/cost)
